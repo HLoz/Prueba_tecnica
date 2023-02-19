8eba1e402221f2d04da2fca5f9548e465d2bfb6d
HLoz <hectorlozano87@gmail.com>
2023-02-19 05:29:48 +0100

    Creada funcion salarios en models.py

--- app_rrhh/models.py
+++ app_rrhh/models.py
@@ -21,3 +21,25 @@ def personas():
     
     print(f"La empresa esta compuesta por {total_H_y_M} trabajadores, de los cuales:\n-Hombres son {contH}.\n-Mujeres son {contM}.")
 
+def salarios():
+    data = open(ORIGIN_DB,"r")
+    data = csv.reader(data, delimiter=";", quotechar='"')
+    next(data)
+
+    salarios = []#pos6
+    sBruto_empresa1 = 0#pos7
+    salario_ct2 = []
+    sBruto_ct2 = 0#pos9
+
+    for registro in data:
+        if registro[7] == "1":
+            salarios.append(float(registro[6]))
+            sBruto_empresa1 = sum(salarios)
+    
+        if registro[9] == "CT2":
+            salario_ct2.append(float(registro[6]))
+            sBruto_ct2 = sum(salario_ct2)
+            sBruto_ct2 = sBruto_ct2
+           
+    print(f"El salario bruto  anual de los empleados de la Empresa 1 (Equilibra IT) es de: {sBruto_empresa1} €.")    
+    print(f"El salario bruto  anual de los empleados del Centro de Alovera (CT2) es de: {sBruto_ct2} €.")
--- app_rrhh/views.py
+++ app_rrhh/views.py
@@ -1,3 +1,4 @@
 from models import *
 
 people = personas()
+salario = salarios()
